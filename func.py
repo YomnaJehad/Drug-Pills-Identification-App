fb444f17974542bf8a56a6fbe462b06a23e67bff
Ahmed Ebeid <a.ebeid@hotmail.com>
2019-04-22 13:16:22 +0200

    3shan morsi

--- func.py
+++ func.py
@@ -28,13 +28,31 @@ def rgb2hsv(r, g, b):
 
 def detectShape(path):
 	img= cv2.imread(path,1)
-
 	img = cv2.resize(img, (0, 0), None, .5, .5)
+
+	img = cv2.GaussianBlur(img, (3,3), 0)
+	kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
+	img = cv2.filter2D(img, -1, kernel)
+
 	drugShape = 'UNDEFINED'
 	edges = cv2.Canny(img,100,200)
 	contours, hierarchy = cv2.findContours(edges,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+
+	areas = np.array([cv2.contourArea(c) for c in contours])
+	contours = np.array(contours)
+	arr1inds = areas.argsort()
+	contours = contours[arr1inds[::-1]]
+	
+
 	approx = cv2.approxPolyDP(contours[0], 0.01*cv2.arcLength(contours[0],True), True)	
 	x,y,w,h = cv2.boundingRect(contours[0]) # offsets - with this you get 'mask'
+
+
+	cv2.drawContours(img, [contours[0]], 0,(255,0,0), 2)
+	cv2.imshow(path, img)
+
+
+
 	newIM = img[y:y+h,x:x+w]
 	yn = newIM.shape[0]
 	xn = newIM.shape[1]
@@ -49,11 +67,11 @@ def detectShape(path):
 	prediction = 'n.a.'
 
 	# using rbg
-	# prediction = knn_classifier.main('training.data', np.array([colors[2], colors[1], colors[0]]))
+	prediction = knn_classifier.main('training.data', np.array([colors[2], colors[1], colors[0]]))
 
 	# using hsv
-	colors = rgb2hsv(colors[2], colors[1], colors[0])
-	prediction = knn_classifier.main('newData.data', np.array([colors[0], colors[1], colors[2]]))
+	# colors = rgb2hsv(colors[2], colors[1], colors[0])
+	# prediction = knn_classifier.main('newData.data', np.array([colors[0], colors[1], colors[2]]))
 	# print(colors[2], colors[1], colors[0])
 
 	return len(approx), prediction, contours[0]
@@ -64,8 +82,8 @@ def detectDrug(path):
 	# print(lenn,color)
 	drugShape= 'UNDEFINED'
 	drugName= 'UNDEFINED'
-	
-	if 6 < lenn < 13:
+	print(lenn)
+	if 5 < lenn < 13:
 		drugShape = 'Ellipse'
 	elif lenn == 6:
 		drugShape = 'Hexagon'
@@ -87,13 +105,26 @@ def detectDrug(path):
 
 	return drugShape, color
 
+print('circle', detectDrug('PanLine.jpeg'))
+print('circle', detectDrug('PanAE.jpeg'))
+print('circle', detectDrug('57272104_2251544638493290_760916686994931712_n.jpg'))
 
-print('circle', detectDrug('circle.png'))
-print('ellipse', detectDrug('ellipse.png'))
-print('hexagon', detectDrug('hexagon.png'))
-print('rec', detectDrug('rec.jpg'))
-print('square', detectDrug('square.jpeg'))
-print('pentagon', detectDrug('pentagon.jpg'))
+print('cataflam1', detectDrug('Shapes/Cataflam/1.jpg'))
+print('cataflam2', detectDrug('Shapes/Cataflam/2.jpg'))
+print('cataflam3', detectDrug('Shapes/Cataflam/3.jpg'))
+print('cataflam4', detectDrug('Shapes/Cataflam/4.jpg'))
+print('cataflam5', detectDrug('Shapes/Cataflam/5.jpg'))
+print('cataflam6', detectDrug('Shapes/Cataflam/6.jpg'))
+print('cataflam7', detectDrug('Shapes/Cataflam/7.jpg'))
+print('cataflam8', detectDrug('Shapes/Cataflam/8.jpg'))
+print('cataflam9', detectDrug('Shapes/Cataflam/9.jpg'))
+print('cataflam10', detectDrug('Shapes/Cataflam/10.jpg'))
+print('cataflam11', detectDrug('Shapes/Cataflam/11.jpg'))
+
+# print('hexagon', detectDrug('hexagon.png'))
+# print('rec', detectDrug('rec.jpg'))
+# print('square', detectDrug('square.jpeg'))
+# print('pentagon', detectDrug('pentagon.jpg'))
 
 # print(detectDrug('/media/yomna/New Volume/2nd term/Project/PanLine.jpeg'))
 
